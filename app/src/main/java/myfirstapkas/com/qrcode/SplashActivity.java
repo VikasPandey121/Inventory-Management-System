43b132066f6f844a98819f8062015ff4bad07507
aanuj67950 <aanuj67950@gmail.com>
2020-01-24 15:12:59 +0530

    Added New Splash Activity

--- app/src/main/java/myfirstapkas/com/qrcode/SplashActivity.java
+++ app/src/main/java/myfirstapkas/com/qrcode/SplashActivity.java
@@ -2,36 +2,61 @@ package myfirstapkas.com.qrcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 import com.pushbots.push.Pushbots;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private ImageView logo;
+    private static  int SPLASH_SCREEN = 4000;
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        logo=(ImageView)findViewById(R.id.imageView);
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
-        Animation myanim = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.shade);
-        logo.startAnimation(myanim);
+        image = findViewById(R.id.ImageView);
+        logo = findViewById(R.id.textView);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this,signupActivity.class);
-               startActivity(intent);
-                   // finish();
+                Intent intent = new Intent(SplashActivity.this,loginActivity.class);
+
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image, "logo_image");
+                pairs[1] = new Pair<View,String>(logo, "logo_text");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SplashActivity.this,pairs);
+                    startActivity(intent,options.toBundle());
+
+                }
             }
-        },2000);
+        } ,SPLASH_SCREEN);
 
     }
+
+
+
 }
